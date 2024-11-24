cb3a558b762df45efc1c7a401935ae16a488ce86
Shota Tevdorashvili <shotatevdorashvilibusiness@gmail.com>
2024-11-24 10:45:40 +0400

    Init Commit

---
+++ src/main/java/me/shotatevdorashvili/minecraftTntGamePlugin/MinecraftTntGamePlugin.java
@@ -0,0 +1,12 @@
+package me.shotatevdorashvili.minecraftTntGamePlugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MinecraftTntGamePlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+}
