8f87b37d3c7e333a808c7c0b81611425e00c758a
Shota Tevdorashvili <shotatevdorashvilibusiness@gmail.com>
2024-11-24 23:00:45 +0400

    changed madebyshota colors

--- src/main/java/me/shotatevdorashvili/minecraftTntGamePlugin/Listeners/MadeByShotaTevdorashvili.java
+++ src/main/java/me/shotatevdorashvili/minecraftTntGamePlugin/Listeners/MadeByShotaTevdorashvili.java
@@ -1,7 +1,6 @@
 package me.shotatevdorashvili.minecraftTntGamePlugin.Listeners;
 
 import org.bukkit.ChatColor;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -10,8 +9,6 @@ public class MadeByShotaTevdorashvili implements Listener {
 
     @EventHandler
     public void onJoin(PlayerJoinEvent e) {
-        Player player = e.getPlayer();
-
-        e.setJoinMessage(ChatColor.WHITE + "This Plugis was made by " + ChatColor.GOLD + "Shota Tevdorashvili " + ChatColor.GOLD + " https://github.com/Sh0utaa/MinecraftTntGamePlugin");
+        e.setJoinMessage(ChatColor.WHITE + "This plugin was made by " + ChatColor.GOLD + "Shota Tevdorashvili https://github.com/Sh0utaa/MinecraftTntGamePlugin");
     }
 }
